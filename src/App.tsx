5d95ff247075b39e50bf0d02aa53a0fa9229daa6
Gabriel Romay Machado <romay.gabriel@gmail.com>
2022-01-26 16:51:50 +0100

    Fix App.test

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./App.css";
 
 // import ClassComponent from "./components/atoms/ClassComponent";
+// import UncontrolledForm from "./components/atoms/UncontrolledForm";
 import UseStateForm from "./components/atoms/UseStateForm";
-import UncontrolledForm from "./components/atoms/UncontrolledForm";
 
 function App() {
   return (
     <div className="App">
+      <h1> Some React Concepts examples </h1>
       {/* <ClassComponent name="Gabriel" /> */}
+      {/* <UncontrolledForm /> */}
       <UseStateForm />
-      <UncontrolledForm />
     </div>
   );
 }

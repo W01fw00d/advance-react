c1031c7aaf3ff002063cbe666d7b2caf32a16e2c
Gabriel Romay Machado <romay.gabriel@gmail.com>
2022-01-27 10:39:59 +0100

    Improve flatten

--- jsScripts/Flatten.js
+++ jsScripts/Flatten.js
@@ -6,31 +6,39 @@ const multipleDepthsObject = {
     email: "pedro.fake@gmail.com",
     age: "50",
   },
-  /* address: {
-    street: {
+  active: true,
+  address: {
+    /* street: {
       name: "Street",
       number: "11",
-    },
+    }, */
     PC: "000000",
-  }, */
-  active: true,
+  },
   // itemsBought: ["TV", "Box", "Sofa"], // TODO: flatten arrays like "itemsBought1: "TV"
 };
 
-const flatten = (objectToFlatten) => {
+const flatten = (objectToFlatten, previousKey) => {
   const keys = Object.keys(objectToFlatten);
   console.log({ keys });
 
   const result = keys.reduce((total, currentKey) => {
-    console.log({ total, currentKey });
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const combinedCurrentKey = previousKey
+      ? previousKey + capitalizeFirstLetter(currentKey)
+      : currentKey;
+
+    console.log({ total, combinedCurrentKey });
 
     const currentValue = objectToFlatten[currentKey];
     console.log({ currentValue });
 
     if (typeof currentValue === "object") {
-      return { ...total, ...flatten(currentValue) };
+      return { ...total, ...flatten(currentValue, currentKey) };
     } else {
-      total[currentKey] = currentValue;
+      total[combinedCurrentKey] = currentValue;
       return total;
     }
   }, {});
